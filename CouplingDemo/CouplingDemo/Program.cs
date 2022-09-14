5c4efad1dce99d991d2684aef4faf9dc153bf2de
Cesar Antonio Hernandez Valtierra <86888362+CesarBlvD@users.noreply.github.com>
2022-09-14 13:46:39 -0500

    acoplamiento

---
+++ CouplingDemo/CouplingDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CouplingDemo
+{
+    class Program
+    {
+        public Program()
+        {
+            var deviceManager = new DeviceManager();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class DeviceManager 
+    {
+         public DeviceManager()
+        {
+            var serviceManager = new ServiceManager();
+        }
+    }
+
+    class ServiceManager 
+    {
+        
+    }
+}
