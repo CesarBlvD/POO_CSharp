fe242ec505fe157a44c109dafec76c57d5710c1d
Cesar Antonio Hernandez Valtierra <86888362+CesarBlvD@users.noreply.github.com>
2022-09-14 11:07:30 -0500

    Inicio de Store

---
+++ Csharp/TheStore/TheStore.BL/Customer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheStore.BL
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        #region Metodos
+        //Save
+        public bool Save(Customer newCustomer)
+        {
+
+            return true;
+        }
+        //Get All
+        public List<Customer> GetAll()
+        {
+
+            return new List<Customer>();
+        }
+        //Get By Id
+        public Customer GetById(int id)
+        {
+            return new Customer();
+        }
+        //Delete
+        public bool Delete(int id)
+        {
+            return true;
+        }
+
+
+        #endregion Metodos
+
+    }
+}
---
+++ Csharp/TheStore/TheStore.BL/OrderDetail.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheStore.BL
+{
+    public class OrderDetail
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public decimal PurchasePrice { get; set; }
+
+        #region Metodos
+
+        public bool Save(OrderDetail newOrder)
+        {
+            return true;
+        }
+        public List<OrderDetail> GetAll()
+        {
+            return new List<OrderDetail>();
+        }
+        public bool Delete(int id)
+        {
+            return true;
+        }
+
+
+        #endregion Metodos
+    }
+}
